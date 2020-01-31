bce36a441a5f187c49016b7df080f36c68269c1c
Giorgio Acquati <gioac96@gmail.com>
2020-01-31 16:18:39 +0100

    Fix project setup

--- src/test/java/org/gioac96/veronica/samples/Echo.java
+++ src/test/java/org/gioac96/veronica/samples/Echo.java
@@ -1,8 +1,6 @@
 package org.gioac96.veronica.samples;
 
-import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
-import java.net.InetSocketAddress;
 import org.gioac96.veronica.Application;
 
 public class Echo {
