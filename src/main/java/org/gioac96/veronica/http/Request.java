a469daf95d9ad0d6cf0e82aed43762d655fa3a57
Giorgio Acquati <gioac96@gmail.com>
2020-01-31 23:54:03 +0100

    Add support for cookies and query params

--- src/main/java/org/gioac96/veronica/http/Request.java
+++ src/main/java/org/gioac96/veronica/http/Request.java
@@ -1,23 +1,103 @@
 package org.gioac96.veronica.http;
 
+import com.sun.net.httpserver.Headers;
+import java.net.HttpCookie;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
-@Builder
 public class Request {
 
     @Getter
+    @NonNull
     protected final HttpMethod httpMethod;
 
     @Getter
+    @NonNull
     protected final String body;
 
-    public Request(HttpMethod httpMethod, String body) {
+    @Getter
+    @NonNull
+    protected final Headers headers;
+
+    @Getter
+    @NonNull
+    protected final URI uri;
+
+    @Getter(lazy = true)
+    private final transient Map<String, String> queryMap = lazyLoadQueryMap();
+
+    @Getter(lazy = true)
+    private final transient List<HttpCookie> cookie = lazyLoadCookie();
+
+    @Builder
+    public Request(
+        @NonNull HttpMethod httpMethod,
+        @NonNull String body,
+        @NonNull Headers headers,
+        @NonNull URI uri
+    ) {
 
         this.httpMethod = httpMethod;
         this.body = body;
+        this.headers = headers;
+        this.uri = uri;
 
     }
 
+    public String getQueryParam(String paramName) {
+
+        return getQueryMap().getOrDefault(paramName, null);
+
+    }
+
+    public String getPath() {
+
+        return uri.getPath();
+
+    }
+
+    private Map<String, String> lazyLoadQueryMap() {
+
+        Map<String, String> queryMap = new HashMap<>();
+
+        if (uri.getQuery() == null || uri.getQuery().length() == 0) {
+
+            return queryMap;
+
+        }
+
+        String[] queryParts = uri.getQuery().split("&");
+
+        for (String queryPart : queryParts) {
+
+            String[] queryParamParts = queryPart.split("=");
+
+            if (queryParamParts.length > 1) {
+
+                queryMap.put(queryParamParts[0], queryParamParts[1]);
+
+            } else {
+
+                queryMap.put(queryParamParts[0], "");
+
+            }
+
+        }
+
+        return queryMap;
+
+    }
+
+    private List<HttpCookie> lazyLoadCookie() {
+
+        return HttpCookie.parse(headers.getFirst("cookie"));
+
+    }
 
 }
