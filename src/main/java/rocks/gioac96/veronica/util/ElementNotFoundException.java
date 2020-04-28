6082cfa6321fbab76d2cd0940b3e9e8c1ec2eba4
Giorgio Acquati <gioac96@gmail.com>
2020-04-28 16:13:10 +0200

    Fix checkstyle

--- src/main/java/rocks/gioac96/veronica/util/ElementNotFoundException.java
+++ src/main/java/rocks/gioac96/veronica/util/ElementNotFoundException.java
@@ -2,6 +2,9 @@ package rocks.gioac96.veronica.util;
 
 import lombok.AllArgsConstructor;
 
+/**
+ * Exception thrown when a PrioritySet element was not found inside the set.
+ */
 @AllArgsConstructor
 public final class ElementNotFoundException extends Exception {
 
