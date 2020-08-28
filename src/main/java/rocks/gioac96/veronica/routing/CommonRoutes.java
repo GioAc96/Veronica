3986f4c8cfe3e2ebd606e93ddeccb6fb763c1e70
Giorgio Acquati <gioac96@gmail.com>
2020-08-28 10:53:10 +0200

    Add  common routes

--- src/main/java/rocks/gioac96/veronica/routing/CommonRoutes.java
+++ src/main/java/rocks/gioac96/veronica/routing/CommonRoutes.java
@@ -1,6 +1,9 @@
 package rocks.gioac96.veronica.routing;
 
+import static rocks.gioac96.veronica.routing.pipeline.stages.RequestHandlerPayload.ok;
+
 import lombok.experimental.UtilityClass;
+import rocks.gioac96.veronica.http.CommonResponses;
 import rocks.gioac96.veronica.http.HttpStatus;
 import rocks.gioac96.veronica.http.Request;
 import rocks.gioac96.veronica.http.Response;
@@ -11,6 +14,15 @@ import rocks.gioac96.veronica.routing.pipeline.stages.RequestHandlerPayload;
  */
 @UtilityClass
 public class CommonRoutes {
+
+    private Route<? extends Request, ? extends Response> emptyError(HttpStatus httpStatus) {
+
+        return Route.builder()
+            .alwaysMatch()
+            .requestHandler(request -> ok(CommonResponses.empty(httpStatus)))
+            .build();
+
+    }
 
     /**
      * Instantiates a {@link Route} that redirects http requests to https.
@@ -24,10 +36,32 @@ public class CommonRoutes {
             .requestHandler(request -> RequestHandlerPayload.ok(Response.builder()
                 .httpStatus(HttpStatus.MOVED_PERMANENTLY)
                 .header("Location", request.getUri().toString().replaceFirst("^http", "https"))
-                .body("")
+                .emptyBody()
                 .build())
             )
             .build();
+
+    }
+
+    /**
+     * Instantiates a {@link Route} that always returns an http "NOT FOUND" error.
+     *
+     * @return the instantiated route
+     */
+    public Route<? extends Request, ? extends Response> notFound() {
+
+        return emptyError(HttpStatus.NOT_FOUND);
+
+    }
+
+    /**
+     * Instantiates a {@link Route} that always returns an http "INTERNAL SERVER ERROR" error.
+     *
+     * @return the instantiated route
+     */
+    public Route<? extends Request, ? extends Response> internalError() {
+
+        return emptyError(HttpStatus.INTERNAL_SERVER_ERROR);
 
     }
 
--- src/main/java/rocks/gioac96/veronica/routing/Route.java
+++ src/main/java/rocks/gioac96/veronica/routing/Route.java
@@ -1,30 +1,28 @@
 package rocks.gioac96.veronica.routing;
 
+import java.util.function.Function;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import lombok.experimental.SuperBuilder;
 import rocks.gioac96.veronica.http.Request;
 import rocks.gioac96.veronica.http.Response;
 import rocks.gioac96.veronica.routing.matching.CommonRequestMatchers;
 import rocks.gioac96.veronica.routing.matching.RequestMatcher;
 import rocks.gioac96.veronica.routing.pipeline.Pipeline;
 import rocks.gioac96.veronica.routing.pipeline.stages.RequestHandler;
+import rocks.gioac96.veronica.routing.pipeline.stages.RequestHandlerPayload;
 
 /**
  * Application route.
  */
-@SuperBuilder
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class Route<Q extends Request, S extends Response> {
 
     @Getter
     @Setter
     @NonNull
-    @Builder.Default
     private RequestMatcher<Q> requestMatcher = CommonRequestMatchers.neverMatch();
 
     @Getter
@@ -35,8 +33,17 @@ public class Route<Q extends Request, S extends Response> {
     @Getter
     @Setter
     @NonNull
-    @Builder.Default
     private Pipeline<Q, S> pipeline = Pipeline.<Q, S>builder().build();
+
+    protected Route(RouteBuilder<Q, S, ?, ?> b) {
+        this.requestMatcher = b.requestMatcher;
+        this.requestHandler = b.requestHandler;
+        this.pipeline = b.pipeline;
+    }
+
+    public static <Q extends Request, S extends Response> RouteBuilder<Q, S, ?, ?> builder() {
+        return new RouteBuilderImpl<Q, S>();
+    }
 
     /**
      * Checks whether the route should handle the specified {@link Request}.
@@ -62,4 +69,75 @@ public class Route<Q extends Request, S extends Response> {
 
     }
 
+    @SuppressWarnings({"checkstyle:ModifierOrder", "checkstyle:MissingJavadocType", "checkstyle:MissingJavadocMethod"})
+    public static abstract class RouteBuilder<
+        Q extends Request,
+        S extends Response,
+        C extends Route<Q, S>,
+        B extends RouteBuilder<Q, S, C, B>
+        > {
+
+        @NonNull
+        private RequestMatcher<Q> requestMatcher;
+
+        @NonNull
+        private RequestHandler<Q, S> requestHandler;
+
+        @NonNull
+        private Pipeline<Q, S> pipeline;
+
+        public B requestMatcher(@NonNull RequestMatcher<Q> requestMatcher) {
+
+            this.requestMatcher = requestMatcher;
+            return self();
+
+        }
+
+        public B alwaysMatch() {
+
+            return requestMatcher(CommonRequestMatchers.alwaysMatch());
+
+        }
+
+        public B requestHandler(@NonNull RequestHandler<Q, S> requestHandler) {
+
+            this.requestHandler = requestHandler;
+            return self();
+
+        }
+
+        public B pipeline(@NonNull Pipeline<Q, S> pipeline) {
+
+            this.pipeline = pipeline;
+            return self();
+
+        }
+
+        protected abstract B self();
+
+        public abstract C build();
+
+    }
+
+    private static final class RouteBuilderImpl<
+        Q extends Request,
+        S extends Response
+        > extends RouteBuilder<
+        Q,
+        S,
+        Route<Q, S>,
+        RouteBuilderImpl<Q, S>
+        > {
+
+        private RouteBuilderImpl() {
+        }
+
+        protected Route.RouteBuilderImpl<Q, S> self() {
+            return this;
+        }
+
+        public Route<Q, S> build() {
+            return new Route<Q, S>(this);
+        }
+    }
 }
