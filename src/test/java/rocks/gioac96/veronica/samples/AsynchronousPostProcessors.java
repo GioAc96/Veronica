d5de3fbac34c6678df4e73f3dc17de83d064ff64
Giorgio Acquati <gioac96@gmail.com>
2020-08-29 13:38:51 +0200

    Minor improvements

--- src/test/java/rocks/gioac96/veronica/samples/AsynchronousPostProcessors.java
+++ src/test/java/rocks/gioac96/veronica/samples/AsynchronousPostProcessors.java
@@ -1,6 +1,7 @@
 package rocks.gioac96.veronica.samples;
 
 import static rocks.gioac96.veronica.routing.matching.CommonRequestMatchers.favicon;
+import static rocks.gioac96.veronica.routing.matching.CommonRequestMatchers.get;
 import static rocks.gioac96.veronica.routing.pipeline.stages.RequestHandlerPayload.ok;
 
 import rocks.gioac96.veronica.Application;
@@ -8,6 +9,7 @@ import rocks.gioac96.veronica.Server;
 import rocks.gioac96.veronica.http.CommonResponses;
 import rocks.gioac96.veronica.http.Request;
 import rocks.gioac96.veronica.http.Response;
+import rocks.gioac96.veronica.routing.CommonRoutes;
 import rocks.gioac96.veronica.routing.Route;
 import rocks.gioac96.veronica.routing.Router;
 import rocks.gioac96.veronica.routing.pipeline.Pipeline;
@@ -15,32 +17,41 @@ import rocks.gioac96.veronica.routing.pipeline.stages.AsynchronousPostProcessor;
 
 public class AsynchronousPostProcessors {
 
+    private static void sleep() {
+
+        try {
+
+            Thread.sleep(2000);
+
+            System.out.println("Finished");
+
+        } catch (InterruptedException ignored) {}
+
+    }
+
     public static void main(String[] args) {
 
         Application.basic()
             .server(Server.builder().port(80).build())
             .threads(1)
             .router(Router.builder()
-                .route(
-                    Route.builder()
-                        .requestMatcher(favicon())
-                        .requestHandler(request -> ok(CommonResponses.notFound()))
-                        .build()
-                )
-                .fallbackRoute(Route.builder()
+                .route(Route.builder()
+                    .requestMatcher(get("/async"))
                     .requestHandler(request -> ok(CommonResponses.ok()))
                     .pipeline(
                         Pipeline.builder()
-                            .postProcessor((AsynchronousPostProcessor<Request, Response>)(request, response) -> {
-                                try {
-                                    Thread.sleep(2000);
-                                    System.out.println("Finished post processor");
-                                } catch (InterruptedException e) {
-                                    e.printStackTrace();
-                                }
-                            })
+                            .postProcessor((AsynchronousPostProcessor<Request, Response>)(request, response) -> sleep())
                             .build())
                     .build())
+                .route(Route.builder()
+                    .requestMatcher(get("/sync"))
+                    .requestHandler(request -> ok(CommonResponses.ok()))
+                    .pipeline(
+                        Pipeline.builder()
+                            .postProcessor((request, response) -> sleep())
+                            .build())
+                    .build())
+                .fallbackRoute(CommonRoutes.notFound())
                 .build())
             .build()
             .start();
