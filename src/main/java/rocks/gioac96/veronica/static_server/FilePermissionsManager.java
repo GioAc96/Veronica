887382b4223298638dd9187bce154ce8f84ddff2
Giorgio Acquati <gioac96@gmail.com>
2020-09-01 10:49:57 +0200

    Move static server package

--- src/main/java/rocks/gioac96/veronica/static_server/FilePermissionsManager.java
+++ src/main/java/rocks/gioac96/veronica/static_server/FilePermissionsManager.java
@@ -1,4 +1,4 @@
-package rocks.gioac96.veronica.http.static_server;
+package rocks.gioac96.veronica.static_server;
 
 import java.nio.file.Path;
 import java.util.HashSet;
--- src/test/java/rocks/gioac96/veronica/static_server/FilePermissionsManagerTest.java
+++ src/test/java/rocks/gioac96/veronica/static_server/FilePermissionsManagerTest.java
@@ -1,4 +1,4 @@
-package rocks.gioac96.veronica.http.static_server;
+package rocks.gioac96.veronica.static_server;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
