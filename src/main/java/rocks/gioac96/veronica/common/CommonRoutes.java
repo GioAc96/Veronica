d38228e423ba9b3bd8a99a47ae281515656dfefc
Giorgio Acquati <gioac96@gmail.com>
2020-09-29 12:14:21 +0200

    Add no favicon common route to CommonRoutes Helper

--- src/main/java/rocks/gioac96/veronica/common/CommonRoutes.java
+++ src/main/java/rocks/gioac96/veronica/common/CommonRoutes.java
@@ -1,5 +1,6 @@
 package rocks.gioac96.veronica.common;
 
+import rocks.gioac96.veronica.common.routes.NoFavicon;
 import rocks.gioac96.veronica.common.routes.NotFound;
 import rocks.gioac96.veronica.common.routes.RedirectToSecure;
 import rocks.gioac96.veronica.core.Route;
@@ -12,6 +13,7 @@ public final class CommonRoutes {
 
     private static final Provider<Route> redirectToSecure = new RedirectToSecure();
     private static final Provider<Route> notFound = new NotFound();
+    private static final Provider<Route> noFavIcon = new NoFavicon();
 
     public static Route redirectToSecure() {
 
@@ -22,6 +24,12 @@ public final class CommonRoutes {
     public static Route notFound() {
 
         return notFound.provide();
+
+    }
+
+    public static Route noFavIcon() {
+
+        return noFavIcon.provide();
 
     }
 
