051450edf9f319421308ab37d0bd3e1e3bef28e9
Giorgio Acquati <gioac96@gmail.com>
2020-10-05 12:33:34 +0200

    Test RedirectToSecure common routing guard

---
+++ src/test/java/rocks/gioac96/veronica/common/routing_guards/RedirectToSecureTest.java
@@ -0,0 +1,96 @@
+package rocks.gioac96.veronica.common.routing_guards;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.net.URI;
+import org.junit.jupiter.api.Test;
+import rocks.gioac96.veronica.core.HttpStatus;
+import rocks.gioac96.veronica.core.Request;
+import rocks.gioac96.veronica.core.Response;
+import rocks.gioac96.veronica.core.RoutingGuard;
+
+class RedirectToSecureTest {
+
+    private static final RoutingGuard permanentRedirectToSecure = new RedirectToSecure()
+        .permanent()
+        .build();
+
+    private static final RoutingGuard temporaryRedirectToSecure = new RedirectToSecure()
+        .temporary()
+        .build();
+
+    private static final Request secureRequest = mockRequest(true);
+    private static final Request nonSecureRequest = mockRequest(false);
+
+    private static final String secureUri = "https://test.com/";
+    private static final String nonSecureUri = "http://test.com/";
+
+    private static Request mockRequest(boolean isSecure) {
+
+        Request requestMock = mock(Request.class);
+
+        when(requestMock.isSecure()).thenReturn(isSecure);
+
+        URI uriMock = mock(URI.class);
+
+        when(requestMock.getUri()).thenReturn(uriMock);
+
+        if (isSecure) {
+
+            when(uriMock.toString()).thenReturn(secureUri);
+
+        } else {
+
+            when(uriMock.toString()).thenReturn(nonSecureUri);
+
+        }
+
+        return requestMock;
+
+    }
+
+    @Test
+    void testPermanentRedirectSecure() {
+
+        assertNull(
+            permanentRedirectToSecure.handle(secureRequest)
+        );
+
+    }
+
+    @Test
+    void testTemporaryRedirectSecure() {
+
+        assertNull(
+            temporaryRedirectToSecure.handle(secureRequest)
+        );
+
+    }
+    @Test
+    void testPermanentRedirectNonSecure() {
+
+        Response response = permanentRedirectToSecure.handle(nonSecureRequest);
+
+        String locationHeader = response.getHeaders().getFirst("Location");
+
+        assertEquals(secureUri, locationHeader);
+        assertEquals(HttpStatus.MOVED_PERMANENTLY, response.getHttpStatus());
+
+    }
+
+    @Test
+    void testTemporaryRedirectNonSecure() {
+
+        Response response = temporaryRedirectToSecure.handle(nonSecureRequest);
+
+        String locationHeader = response.getHeaders().getFirst("Location");
+
+        assertEquals(secureUri, locationHeader);
+        assertEquals(HttpStatus.TEMPORARY_REDIRECT, response.getHttpStatus());
+
+    }
+
+}
