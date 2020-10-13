6585b9104ebd6e82578f38b27fd62c6a3c471c35
Giorgio Acquati <gioac96@gmail.com>
2020-10-13 16:21:02 +0200

    Minor improvements

--- src/test/java/rocks/gioac96/veronica/tutorials/HelloWorld.java
+++ src/test/java/rocks/gioac96/veronica/tutorials/HelloWorld.java
@@ -1,9 +1,6 @@
 package rocks.gioac96.veronica.tutorials;
 
 import rocks.gioac96.veronica.core.Application;
-import rocks.gioac96.veronica.core.Route;
-import rocks.gioac96.veronica.core.Router;
-import rocks.gioac96.veronica.core.Server;
 import rocks.gioac96.veronica.core.Response;
 import rocks.gioac96.veronica.core.RequestHandler;
 
@@ -15,18 +12,11 @@ public class HelloWorld {
             .body("Hello World")
             .build();
 
-
-        Router router = Router.builder()
-            .defaultRequestHandler(helloWorldHandler)
-            .build();
-
         int port = 8000;
 
         Application app = Application.builder()
-            .server(Server.builder()
-                .port(port)
-                .build()
-            ).router(router)
+            .port(port)
+            .requestHandler(helloWorldHandler)
             .build();
 
         app.start();
