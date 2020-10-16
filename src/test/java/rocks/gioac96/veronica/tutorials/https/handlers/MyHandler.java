61f84564c1117c0472e97deca43ee621768268ab
Giorgio Acquati <gioac96@gmail.com>
2020-10-17 00:26:29 +0200

    Minor fix

--- src/test/java/rocks/gioac96/veronica/tutorials/https/handlers/MyHandler.java
+++ src/test/java/rocks/gioac96/veronica/tutorials/https/handlers/MyHandler.java
@@ -2,8 +2,9 @@ package rocks.gioac96.veronica.tutorials.https.handlers;
 
 import rocks.gioac96.veronica.core.RequestHandler;
 import rocks.gioac96.veronica.core.Response;
+import rocks.gioac96.veronica.providers.ConfigurableProvider;
 
-public class MyHandler extends ConfigurableProvider<RequestHandler>  {
+public class MyHandler extends ConfigurableProvider<RequestHandler> {
 
     @Override
     public RequestHandler instantiate() {
