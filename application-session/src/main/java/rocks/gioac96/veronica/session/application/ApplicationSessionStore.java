f3fe9d0a77f0429c5641325cd9077001050a5c60
Giorgio Acquati <gioac96@gmail.com>
2021-03-23 13:02:28 +0100

    Minor change

--- application-session/src/main/java/rocks/gioac96/veronica/session/application/ApplicationSessionStore.java
+++ application-session/src/main/java/rocks/gioac96/veronica/session/application/ApplicationSessionStore.java
@@ -34,8 +34,7 @@ public class ApplicationSessionStore<D> extends CookieSessionStore<D> {
 
 
     }
-
-
+    
     public static <D> ApplicationSessionStorageBuilder<D> builder() {
 
         return new ApplicationSessionStorageBuilder<>();
